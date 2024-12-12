9a240e8f52d62ef8853e760e009fd246aa5a0d9c
ChaitanyaTD <durga.chaitanya@quadbtech.com>
2024-12-13 00:42:11 +0530

    changes

--- funcs/app.js
+++ funcs/app.js
@@ -76,3 +76,7 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+
+app.use("/.netlify/functions/app", router);
+module.exports.handler = serverless(app);
